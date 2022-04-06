b637dea9772fe38d522391be847e638497b7cc13
Moisés <moisesnetored@gmail.com>
2022-04-06 15:37:42 -0400

    add: console.log token

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { useUser } from "@auth0/nextjs-auth0";
+import {GetServerSideProps} from 'next';
+import { getAccessToken, useUser } from "@auth0/nextjs-auth0";
 
 export default function Home (){
   const {user}= useUser();
@@ -16,4 +17,13 @@ export default function Home (){
   )
 }
 
+export const getServerSideProps: GetServerSideProps = async ({req, res}) => {
+  const token = getAccessToken(req, res);
+
+  console.log(token);
+
+  return {
+    props: {}
+  }
+}
 
